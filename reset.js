787b89db85fd51d78bc9f385aa8a260dcff79168
Fishi736 <53339462+Fishi736@users.noreply.github.com>
2020-11-01 17:56:21 +0530

    done

--- reset.js
+++ reset.js
@@ -1,5 +1,5 @@
 const $resetButton = document.getElementById("reset");
 
 $resetButton.onclick = () => {
-    location.replace("/index.html");
+    location.replace(" https://fishi736.github.io/Guess-Game/");
 }
